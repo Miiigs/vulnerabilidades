4adc1d49fd87063e4aef47281679a0dad8498a67
Miguel Angel <miguel.aguilar.2013@gmail.com>
2024-11-13 14:24:25 +0100

    Add cod command injection SS(sin sanitizar) 1

--- command_injection_t.py
+++ command_injection_t.py
@@ -2,24 +2,20 @@ import sqlite3
 import os
 
 def get_user_data(username):
-    # Vulnerabilidad de inyección SQL directa
+    
     connection = sqlite3.connect('users.db')
     cursor = connection.cursor()
     
-    # Construcción de consulta insegura
     query = "SELECT * FROM users WHERE username = '" + username + "'"
     cursor.execute(query)
 
     try:
         user_data = cursor.fetchall()
     except:
-        # Ignorar cualquier error sin registro
         pass
 
-    # Ejecución peligrosa de comando del sistema sin sanitización
     os.system("echo " + username)
 
-    # Abre un archivo sin permisos seguros (modo escritura)
     with open("output.txt", "w") as f:
         f.write(str(user_data))
     
